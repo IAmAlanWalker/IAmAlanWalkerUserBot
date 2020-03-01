f571d3aa23cd26af702ef056fd1b99c93fd87f96
HacͥҜeͣrͫ✨ <61057531+Hack12R@users.noreply.github.com>
2020-03-01 10:44:31 -0800

    Add files via upload

---
+++ userbot/plugins/smd.py
@@ -0,0 +1,425 @@
+"""
+Say something interesting...
+Syntax: .smd
+    by @Hack12R
+Dialogues credit: To their Writers Sed
+"""
+
+from telethon import events
+
+import asyncio
+
+import os
+
+import sys
+
+import random
+
+from uniborg.util import admin_cmd
+
+
+
+@borg.on(admin_cmd(pattern=r"smd"))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    await event.edit("Typing...")
+
+    await asyncio.sleep(2)
+
+    x=(random.randrange(1,96))
+
+    if x==1:
+
+        await event.edit("`\"Mata ji yahan hai, Behen yaha hai, Maa-Behen ek karne mein aasani hogi.\"`")
+
+    if x==2:
+
+        await event.edit("`\"Attack me bhi gun, defense me bhi gun, hum banayenge Mirzapur ko Amirica.\"`")
+
+    if x==3:
+
+        await event.edit("`\"Attack me bhi peru, defense me bhi peru, hum banayenge Teleram ko PeruPlace.\"`")
+
+    if x==4:
+
+        await event.edit("`\"Neta banna hai toh gundey paalo. Gundey math bano.\"`")
+
+    if x==5:
+
+        await event.edit("`\"Darr ki yahi dikkat hain, ki kabhi bhi Khatam ho sakta hai.\"`")
+
+    if x==6:
+
+        await event.edit("`\"Guns ki maddad se darr nahi badhana hai, darr ki maddad se guns badhani hai.\"`")
+
+    if x==7:
+
+        await event.edit("`\"Ye madam ji sirf tabhi khayengi, jab ye date pe aayengi.\"`")
+
+    if x==8:
+
+        await event.edit("`\"Suru majburi mein kiye the…..Ab maza aa rahe hain.\"`")
+
+    if x==9:
+
+        await event.edit("`\" Tumhare chutiyape kab kahtam hongen.\"`")
+
+    if x==10:
+
+        await event.edit("`\"Har bar garam karke Thanda hi chhod dete ho.\"`")
+
+    if x==11:
+
+        await event.edit("`\"Ch***ya hain Tumhara Beta.\"`")
+
+    if x==12:
+
+        await event.edit("`\"Ch***ye bc.\"`")
+
+    if x==13:
+
+        await event.edit("`\"Ch***ya hain woh important nahi. Hamara beta hain, who important hain.\"`")
+
+    if x==14:
+
+        await event.edit("`\"Hum toh A.K. Hangal samaj ke aye. Aap toh be**c**h Bachchan nikley.\"`")
+
+    if x==15:
+
+        await event.edit("`\"Oh Bho***Iwaley Chacha. Rest kariye, warna Rest in Peace ho jayoge.\"`")
+
+    if x==16:
+
+        await event.edit("`\"ab desh ke PM ka imaan nahin toh apun sidhe raaste chal ke kya karega.\"`")
+
+    if x==17:
+
+        await event.edit("`\"विशुद्ध #$@#! लड़के हो तुम.\"`")
+
+    if x==18:
+
+        await event.edit("`\"अबे बवाल चीज है बे. साला सिस्टम हिल जाता है.\"`")
+
+    if x==19:
+
+        await event.edit("`\"तो !@$)+ के तुम इसके लिए हमारे दरवाजे पे चले आए.\"`")
+
+    if x==20:
+
+        await event.edit("`\"अबे अपनी #$@#! न फुला, बता कितना पैसा हो गया.\"`")
+
+    if x==21:
+
+        await event.edit("`\"शादी कोई खाना खाना थोड़े ही न है कि करना ही है.\"`")
+
+    if x==22:
+
+        await event.edit("`\"ये प्यार न साला बहुत $@#! चीज है .\"`")
+
+    if x==23:
+
+        await event.edit("`\"दूरोव भैया का टेलीग्राम ,  दूरोव भैया का ये सब ... भौकाल हो तो ऐसा हो.\"`")
+
+    if x==24:
+
+        await event.edit("`\"फट गई है उसकी. जाने दीजिए.\"`")
+
+    if x==25:
+
+        await event.edit("`\"Bhagwan ko maante ho? Bhagwan ko l*nd farak nahi padta.\"`")
+
+    if x==26:
+
+        await event.edit("`\"Paise ke naam pe aisa dosti hua, jaise hum ek g**nd hagte ho.\"`")
+
+    if x==27:
+
+        await event.edit("`\"Hindustan jab Hindustan nahin bana tha tab se politics ki macchi ko dharam ke tel mein fry karte hue aaye hain.\"`")
+
+    if x==28:
+
+        await event.edit("`\"Teri g*nd mein chhatri daal ke nahin khola na toh mera naam Bunty nahi.\"`")
+
+    if x==29:
+
+        await event.edit("`\"Tum mardon ko aisa kyun lagta hain ki har aurat ko tumhe hi bachana hai.\"`")
+
+    if x==30:
+
+        await event.edit("`\"Kite aadmi se pila kar aayi hai meri bail karane k liye?\"`")
+
+    if x==31:
+
+        await event.edit("`\"Bhagwan aadmi se kahani me baat karta hai. Hum sab ka life ek kahani hai.\"`")
+
+    if x==32:
+
+        await event.edit("`\"Guns, drugs, property sab chota dhandha hai. Asli dhanda hai politics.\"`")
+
+    if x==33:
+
+        await event.edit("`\"Dharmon ka roop yahi hai. Rahgeer ko prem se ghar bulao. Aadar samit bhojan grahan karo. Phir uski aatma pe kabza kar lo.\"`")
+
+    if x==34:
+
+        await event.edit("`\"Gareeb logon ka entry sirf mandir me free hai.\"`")
+
+    if x==35:
+
+        await event.edit("`\"Yeh system, yeh bambai, yeh India, yeh sab kaun hai? Hum hai na bhench*d.\"`")
+
+    if x==36:
+
+        await event.edit("`\"Duniya ke bazaar mein sabse bada dhanda hai dharam. Bhagwan se dara ke chutiya banate hain public ko.\"`")
+
+    if x==37:
+
+        await event.edit("`\"Hindu hotel se apun seekha ki dharam ke naam pe janta ka kitna chutiya banaya ja sakta hai.\"`")
+
+    if x==38:
+
+        await event.edit("`\"Aadmi andar se jitna kaala hota hai, duniya ke samne utna hi safed banne ki koshish karta hai.\"`")
+
+    if x==39:
+
+        await event.edit("`\" Hero banna hai toh pahle comedy chor. Kukko ko sirf action pasand hai.\"`")
+
+    if x==40:
+
+        await event.edit("`\"Mumbai seher hai ye. Kuch bhi ho sakta hai idhar. Gutter ka kutta poore sheher pe moot dega abhi.\"`")
+
+    if x==41:
+
+        await event.edit("`\"Kashmir mein Home Minister ki beti kidnap ho gayi thi, Dilli mein Mandal ke naam pe bacche apne pe kerosene daal rahe the, aur sansad mein roz pradhan mantri aise badal rahe the jaise kachha ho. Yehi toh khel hai sahab.\"`")
+
+    if x==42:
+
+        await event.edit("`\"977 mein Indira Gandhi ki emergency chaalu thi. Sarkar logon ki l*nd kaat ke le jaa reli thi.\"`")
+
+    if x==43:
+
+        await event.edit("`\"Apne l*nd se bahar nikal, nahin toh wanhin rahega zindagi bhar.\"`")
+
+    if x==44:
+
+        await event.edit("`\"Kyun aati hai yeh qurbaani humare zindagi mein baar baar? Allah tang aa gaya hai humse.\"`")
+
+    if x==45:
+
+        await event.edit("`\"Agar mard field pe kaam karena chahe toh passion. Aur agar aurat katna chahe toh bhoot?\"`")
+
+    if x==46:
+
+        await event.edit("`\"Apun Kukoo ke piche bhaag raha tha aur desh mandir ke piche.\"`")
+
+    if x==47:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==48:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==49:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==50:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==51:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==52:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==53:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==54:
+
+        await event.edit("`\"Kabhi kabhi lagta hai ki apun hi bhagwan hai.\"`")
+
+    if x==55:
+
+        await event.edit("`\"Agar heroine hero k sath pahle hi shot me nikal gayi na, toh ghanta picture banegi.\"`")
+
+    if x==56:
+
+        await event.edit("`\"Sirf dil se hi nahi l*ud se bhi khoon nikal raha tha.\"`")
+
+    if x==57:
+
+        await event.edit("`\"Jail me itne kutte jaisi halat thi ki ch*dne ki taakat bhi nahi bachi thi.\"`")
+
+    if x==58:
+
+        await event.edit("`\"Life haram ho gai thi sardar ji jaise ab aapki hone vali hai.\"`")
+
+    if x==59:
+
+        await event.edit("`\"40 din ke baad jab apun vo andhe kue se bahar nikla to bas ek hi kida nach rela tha apun ko bas ga**d marni thi jisne bhi apne saath ye kiya!\"`")
+
+    if x==60:
+
+        await event.edit("`\"Apun ka tip kabhi galat nahi hota sardarji apun bola tha Trivedi bachega.\"`")
+
+    if x==61:
+
+        await event.edit("`\"Pahele baap ne apun ko dar diya, dusre ne dering aur tisre ne sabse jyada jisko pyaar kiya usne dhoka.\"`")
+
+    if x==62:
+
+        await event.edit("`\"Apun ka bambai khatam ho rela tha.\"`")
+
+    if x==63:
+
+        await event.edit("`\"Parle-g khana pad raha hai kali chai me dubo ke.\"`")
+
+    if x==64:
+
+        await event.edit("`\"Apun idhar carrom khel raha hai aur bhai chand pe pahoch gaye.\"`")
+
+    if x==65:
+
+        await event.edit("`\"Abhi lollipop bolo ya chavanpras, tai ke alava koi bhi shortcut nahi tha apun ke pass.\"`")
+
+    if x==66:
+
+        await event.edit("`\"Pradhanmantri se number liya, parliament apne baap ka hai.\"`")
+
+    if x==67:
+
+        await event.edit("`\"Makarsankranti ke din peda hui thi, sab ka katati hai.\"`")
+
+    if x==68:
+
+        await event.edit("`\"Kuchh to baat tha us aadmi me usko apne samay se piche aura age ka malum tha.\"`")
+
+    if x==69:
+
+        await event.edit("`\"Ahum bramhasami.\"`")
+
+    if x==70:
+
+        await event.edit("`\"Ahum bramhasami.\"`")
+
+    if x==71:
+
+        await event.edit("`\"Ahum bramhasami.\"`")
+
+    if x==72:
+
+        await event.edit("`\"Me bhaikhala ka bhai hai tu Goregaon ki gauri, chadar flower mandi me tu milna chori chori.\"`")
+
+    if x==73:
+
+        await event.edit("`\"Tere malik ne tuje sikha ke nahi bheja ki pahele isaa bhai ki izzat karna.\"`")
+
+    if x==74:
+
+        await event.edit("`\"Kuchh log istemaal kate hai aur kuchh sak.\"`")
+
+    if x==75:
+
+        await event.edit("`\"Mat lo sardarji, mat lo, ye guchi baitaal hai ek baar kamar chad gaya to utare nahi utarega.\"`")
+
+    if x==76:
+
+        await event.edit("`\"Tumhe aatmsandeh ka balidaan karna hoga. Mrutyu pe vijay paane ke liye kai baar marna padta hai.\"`")
+
+    if x==77:
+
+        await event.edit("`\"International notice nikala hai Hindustan ne jidhar bhi dekha thok dalenge.\"`")
+
+    if x==78:
+
+        await event.edit("`\"Ye sirf war of religion aur war of nation nahi hai ye war of civilization hai.\"`")
+
+    if x==79:
+
+        await event.edit("`\"Samay ko ham apne kabje me karenge nuclear dhamake se.\"`")
+
+    if x==80:
+
+        await event.edit("`\"Nucleus se nikle the nucleus me hi vaapsa jayenge.\"`")
+
+    if x==81:
+
+        await event.edit("`\"Me bhishan hun. Me brham hu.\"`")
+
+    if x==82:
+
+        await event.edit("`\"Abe vo sala gandu guruji muje pahele se hi zolu lagta tha.\"`")
+
+    if x==83:
+
+        await event.edit("`\"Mujhe aansuon se sakht allergy hai.\"`")
+
+    if x==84:
+
+        await event.edit("`\"Life is a race … if you don’t run fast … you will be like a broken andaa.\"`")
+
+    if x==85:
+
+        await event.edit("`\"Ladkiyon ko IQ nahin CQ chahiye… Cool Quotient.\"`")
+
+    if x==86:
+
+        await event.edit("`\"Boss number kam hai … dimag nahin.\"`")
+
+    if x==87:
+
+        await event.edit("`\"Mere saath kuch bhi karneka… mera ego hurt nahin karneka\"`")
+
+    if x==88:
+
+        await event.edit("`\"Doing something alone is kind of sad, but doing it solo is cool af.\"`")
+
+    if x==89:
+
+        await event.edit("`\"Zindagi mein teen cheezein kabhi underestimate mat karna… I, Me and Myself!\"`")
+
+    if x==90:
+
+        await event.edit("`\"Paida toh main bhi shareef hua tha… Par sharafat se apni kabhi nahin bani.\"`")
+
+    if x==91:
+
+        await event.edit("`\"Kamra ho, naukri ho … toh heech dilwale dulhania le jayenge … nahi toh hum aapke hain koun?.\"`")
+
+    if x==92:
+
+        await event.edit("`\"Bhoot bhoot ... inki maa ki.\"`")
+
+    if x==93:
+
+        await event.edit("`\"Kundan ke pyjame ka naada itna dheela nahi hai ... joh tere blouse ke do hook pe khul jaaye.\"`")
+
+    if x==94:
+
+        await event.edit("`\"Pehli baar mein sabse zyada mazaa aata hai.\"`")
+
+    if x==95:
+
+        await event.edit("`\"Log sunenge to kya kahenge ... chutiya aashiqui ke chakkar mein mar gaya, aur laundiya bhi nahi mili.\"`")     
+
+    if x==96:
+
+        await event.edit("`\"Meri mardangi ke bare mein aap gaon ki kisi bhi ladki se pooch sakte ho ... report achchi milegi.\"`")
+    
+    if x==97:
+
+        await event.edit("`\"Aaj kal pyar na naukrani jaisa ho gaya hai ... aata hai, bell bhajata hai, kaam karta hai aur chala jaata hai.\"`")
+
+    
+
