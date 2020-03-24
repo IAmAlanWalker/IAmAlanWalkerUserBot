dcb5337cf1cc75e67e925185fd85fc638a18f4bd
HacͥҜeͣrͫ✨ <61057531+Hack12R@users.noreply.github.com>
2020-03-24 09:00:03 -0700

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -124,7 +124,7 @@ async def _(event):
                 while response.text == FILLED_UP_DADDY:
                     pack += 1
                     prevv = int(pack) - 1
-                    packname = f"{user.first_name}'s hott Vol.{pack}"
+                    packname = f"{user.first_name}'s hot Vol.{pack}"
                     packshortname = f"Vol._{pack}_with_{userid}"
                     if not await stickerset_exists(bot_conv, packshortname):
                         await event.edit("**Pack No. **" + str(prevv) + "** full! Making a new Pack, Vol. **" + str(pack))
