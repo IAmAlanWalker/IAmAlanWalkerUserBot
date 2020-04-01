4ab052d2a95b9a8cdbd65fb9fe82853ee7b9b4e2
HacͥҜeͣrͫ✨ <61057531+Hack12R@users.noreply.github.com>
2020-04-01 15:26:47 +0530

    Add files via upload

---
+++ userbot/plugins/squ.py
@@ -0,0 +1,43 @@
+# BY @Hack12R
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.squ", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+
+
+    await event.edit("╔═══════════════════╗ \n  \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n \t░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ \t░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ \n╚═══════════════════╝") 
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1) 
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(1)
+    await event.edit("╔═══════════════════╗ \n ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ ░ \n╚═══════════════════╝")
+    await asyncio.sleep(6)
