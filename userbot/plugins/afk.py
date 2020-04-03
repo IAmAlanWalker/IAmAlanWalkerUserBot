c0b3afed4124ede4f3fbb4b3acb1aec88253db80
HacͥҜeͣrͫ✨ <61057531+Hack12R@users.noreply.github.com>
2020-04-04 00:58:33 +0530

    Add files via upload

---
+++ userbot/plugins/afk.py
@@ -0,0 +1,137 @@
+"""AFK Plugin for @UniBorg
+Syntax: .afk REASON"""
+import asyncio
+import datetime
+from telethon import events
+from telethon.tl import functions, types
+
+from sample_config import Config
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+global USER_AFK  
+global afk_time  
+global last_afk_message  
+USER_AFK = {}
+afk_time = None
+last_afk_message = {}
+
+
+@borg.on(events.NewMessage(outgoing=True))  
+async def set_not_afk(event):
+    global USER_AFK  
+    global afk_time  
+    global last_afk_message  
+    current_message = event.message.message
+    if ".afk" not in current_message and "yes" in USER_AFK:  
+        try:
+            await borg.send_message(  
+                Config.PRIVATE_GROUP_BOT_API_ID,  
+                "`Set AFK mode to False`"
+            )
+        except Exception as e:  
+            await borg.send_message(  
+                event.chat_id,
+                "Please set `PRIVATE_GROUP_BOT_API_ID` " + \
+                "for the proper functioning of afk functionality " + \
+                "in @UniBorg\n\n `{}`".format(str(e)),
+                reply_to=event.message.id,
+                silent=True
+            )
+        USER_AFK = {}  
+        afk_time = None  
+
+
+@borg.on(events.NewMessage(pattern=r"\.afk ?(.*)", outgoing=True))  
+async def _(event):
+    if event.fwd_from:
+        return
+    global USER_AFK  
+    global afk_time  
+    global last_afk_message  
+    global reason
+    USER_AFK = {}
+    afk_time = datetime.datetime.now()
+    last_afk_message = {}
+    reason = event.pattern_match.group(1)
+    if not USER_AFK:  
+        last_seen_status = await borg(  
+            functions.account.GetPrivacyRequest(
+                types.InputPrivacyKeyStatusTimestamp()
+            )
+        )
+        if isinstance(last_seen_status.rules, types.PrivacyValueAllowAll):
+            afk_time = datetime.datetime.now()  
+        USER_AFK = f"yes: {reason}"  
+        if reason:
+            await event.edit(f"Set AFK mode to True, and Reason is {reason}")
+        else:
+            await event.edit(f"`Set AFK mode to True`")
+        await asyncio.sleep(5)
+        await event.delete()
+        try:
+            await borg.send_message(  
+                Config.PRIVATE_GROUP_BOT_API_ID,  
+                f"Set AFK mode to True, and Reason is {reason}"
+            )
+        except Exception as e:  
+            logger.warn(str(e))  
+
+
+@borg.on(events.NewMessage(  
+    incoming=True,
+    func=lambda e: bool(e.mentioned or e.is_private)
+))
+async def on_afk(event):
+    if event.fwd_from:
+        return
+    global USER_AFK  
+    global afk_time  
+    global last_afk_message  
+    afk_since = "**a while ago**"
+    current_message_text = event.message.message.lower()
+    if "afk" in current_message_text:
+        # userbot's should not reply to other userbot's
+        # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
+        return False
+    if USER_AFK and not (await event.get_sender()).bot:  
+        if afk_time:
+            afk_since = "" 
+            now = datetime.datetime.now()
+            datime_since_afk = now - afk_time  
+            time = float(datime_since_afk.seconds)
+            days = time // (24 * 3600)
+            time = time % (24 * 3600)
+            hours = time // 3600
+            time %= 3600
+            minutes = time // 60
+            time %= 60
+            seconds = time
+            if days == 1:
+                afk_since = "**Yesterday**"
+            elif days > 1:
+                if days > 6:
+                    date = now + \
+                        datetime.timedelta(
+                            days=-days, hours=-hours, minutes=-minutes)
+                    afk_since = date.strftime("%A, %Y %B %m, %H:%I")
+                else:
+                    wday = now + datetime.timedelta(days=-days)
+                    afk_since = wday.strftime('%A')
+            elif hours > 1:
+                afk_since = f"`{int(hours)} hours {int(minutes)} minutes ago`"
+            elif minutes > 0:
+                afk_since = f"`{int(minutes)} minutes {int(seconds)} secondes ago`"
+            else:
+                afk_since = f"`{int(seconds)} secondes ago`"
+        msg = None
+        message_to_reply = f"`Currently I iz not Here` " + \
+            f"`I iz Offline Now. Cause` **{reason}** (Since: **{afk_since}**)"\
+            if reason \
+            else f"RIP.....\n\n**Important Notice**\n\n[Sorry ! But My Mastor Ded Forever...Sed...Very Sed...](https://telegra.ph//file/a53fa950ff31781d5930a.jpg)"
+        msg = await event.reply(message_to_reply)
+        await asyncio.sleep(5)
+        if event.chat_id in last_afk_message:  
+            await last_afk_message[event.chat_id].delete()  
+        last_afk_message[event.chat_id] = msg  
