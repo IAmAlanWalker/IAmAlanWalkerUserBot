2cdb9462fa24855ed61a9c982ea379085189b218
HacͥҜeͣrͫ✨ <61057531+Hack12R@users.noreply.github.com>
2020-04-04 01:48:17 -0700

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -65,7 +65,7 @@ async def _(event):
         if userid == 1037944593:
             packshortname = "Hack12R_Animated"
         else:
-            packshortname = f"{user.first_name}'s_animated_{pack}" # format: Uni_Borg_userid
+            packshortname = f"Moi_animated_{pack}" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
