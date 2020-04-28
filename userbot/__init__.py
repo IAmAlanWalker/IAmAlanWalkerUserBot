3a3ae04f5dae2d9d94e0adf0fe87a79e448bf81e
HacͥҜeͣrͫ✨ <61057531+Hack12R@users.noreply.github.com>
2020-04-28 05:45:53 -0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -81,6 +81,16 @@ if bool(ENV):
     # Genius lyrics get this value from https://genius.com/developers both has same values
     GENIUS = os.environ.get("GENIUS_API_TOKEN", None)
     
+    # Heroku Credentials for updater.
+    HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+
+    # Custom (forked) repo URL for updater.
+    UPSTREAM_REPO_URL = os.environ.get(
+        "UPSTREAM_REPO_URL",
+        "https://github.com/hackerra/HardcoreUserbot.git")
+    
     # remove.bg API key
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
 
