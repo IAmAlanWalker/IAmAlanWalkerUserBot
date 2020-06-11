f70d28ff625b87dfe3a1207c58fa5aab414bf79d
spandey112 <60309463+spandey112@users.noreply.github.com>
2020-06-11 18:38:10 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -12,7 +12,7 @@ PREV_REPLY_MESSAGE = {}
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No name set yet nibba"
 USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
-USER_BOT_NO_WARN = ("[█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██-----╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗-----██-----║║║╠─║─║─║║║║║╠─-----██-----╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝-----██▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█](tg://user?id=709723121)\n\n"
+USER_BOT_NO_WARN = ("[█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██-----╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗-----██-----║║║╠─║─║─║║║║║╠─-----██-----╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝-----██▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█]"(tg://user?id=709723121)\n\n"
                     "`Hello, this is Ultra Level Telegram Security Service for peru users only.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
                     "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
