53a2fd03666c49646d2106ead26bb2e1bc34b0a6
spandey112 <60309463+spandey112@users.noreply.github.com>
2020-06-11 15:46:49 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -89,7 +89,7 @@ if bool(ENV):
     # Custom (forked) repo URL for updater.
     UPSTREAM_REPO_URL = os.environ.get(
         "UPSTREAM_REPO_URL",
-        "https://github.com/Hack12R/HardcoreUserbot.git")
+        "https://github.com/spandey112/SensibleUserbot.git")
     
     # remove.bg API key
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
