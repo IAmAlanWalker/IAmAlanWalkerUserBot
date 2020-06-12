cfb10702685c0a271f644350de52d7cf1150b135
spandey112 <60309463+spandey112@users.noreply.github.com>
2020-06-12 16:25:04 +0530

    Update ppg.py

--- userbot/plugins/ppg.py
+++ userbot/plugins/ppg.py
@@ -46,8 +46,7 @@ async def _(event):
     except Exception as e:
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
-    caption = """Profile Pic Grabber By @Hack12R
-
+    caption = """Profile Pic Grabber By @ceowhitehatcracks
 Rendi: <a href='tg://user?id={}'>{}</a>
 """.format(
         user_id,
