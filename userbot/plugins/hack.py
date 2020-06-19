ec0bb2e22410f1a430e957ccca77582fcfe387f1
CEO WHITE HAT CRACKS <60309463+spandey112@users.noreply.github.com>
2020-06-19 17:56:28 +0530

    Update hack.py

--- userbot/plugins/hack.py
+++ userbot/plugins/hack.py
@@ -40,10 +40,17 @@ async def _(event):
             "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
             "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 52%\n████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 69$ To` @Ceowhitehatcracks `Or send nudes of your GF To Remove This Hack`"
+            "`TERMINAL Connected....Terminal Name - @ceowhitehatcracks Haxcker Terminal `",
+            "`Downloading Bruteforce-Telegram-0.1.tar.gz (9.3 kB) Collecting Data Package `",
+            "`  Downloading Telegram-Data-Sniffer-7.1.1-py2.py3-none-any.whl (82 kB) `",
+            "`Building wheel for Tg-Bruteforcing (setup.py): finished with status 'done' `",
+            "`Created wheel for telegram: filename=Telegram-Data-Sniffer-0.0.1-py3-none-any.whl size=1306 `",
+            "`sha256=cb224caad7fe01a6649188c62303cd4697c1869fa12d280570bb6ac6a88e6b7e `",
+            "`Stored in directory: /app/.cache/pip/wheels/a2/9f/b5/650dd4d533f0a17ca30cc11120b176643d27e0e1f5c9876b5b Ceo White Hat Cracks/sdcard/hacked `",
+            "`Targeted Account Hacked...\n\nPay 69$ To` @Ceowhitehatcracks `Or send nudes of your GF To Remove This Hack Logs\n TERMINAL:Downloading Bruteforce-Telegram-0.1.tar.gz (9.3 kB)\nCollecting Data Package\n Downloading Telegram-Data-Sniffer-7.1.1-py2.py3-none-any.whl (82 kB)\nBuilding wheel for Tg-Bruteforcing (setup.py): finished with status 'done'\nCreated wheel for telegram: filename=Telegram-Data-Sniffer-0.0.1-py3-none-any.whl size=1306 sha256=cb224caad7fe01a6649188c62303cd4697c1869fa12d280570bb6ac6a88e6b7e\n Stored in directory: /app/.cache/pip/wheels/a2/9f/b5/650dd4d533f0a17ca30cc11120b176643d27e0e1f5c9876b5b\n Successfully Hacked this Account From Telegram Database Stored At @CeowhiteHatcracks Server \n🔹Output: Successful`"
         ]
 
         for i in animation_ttl:
