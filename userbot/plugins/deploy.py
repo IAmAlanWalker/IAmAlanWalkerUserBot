f9f085e4e2cf790bb00934055365464f14b6a38a
CEO WHITE HAT CRACKS <60309463+spandey112@users.noreply.github.com>
2020-06-23 17:57:16 +0530

    Update deploy.py

--- userbot/plugins/deploy.py
+++ userbot/plugins/deploy.py
@@ -35,7 +35,7 @@ async def _(event):
 
     animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (StarkGang/FridayUserbot)**",
+            "**Heroku Connecting To Latest Github Build (Ceo White Hat Cracks/Sensibleerbot)**",
             "**Build started by user** **{DEFAULTUSER}**",
             "**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
             "**Restarting Heroku Server...**",
@@ -44,8 +44,8 @@ async def _(event):
             "**Process exited with** `status 143`",
             "**Starting process with command** `python3 -m stdborg`",
             "**State changed from starting to up**",
-            "__INFO:Friday:Logged in as 557667062__",
-            "__INFO:Friday:Successfully loaded all plugins__",
+            "__INFO:Sensible Userbot:Logged in as 557667062__",
+            "__INFO:Sensible Userbot:Successfully loaded all plugins__",
             "**Build Succeeded**"
 
  ]
