1b0c784f9f2c33a81379330762e4d94ea8a1385b
CEO WHITE HAT CRACKS <official.shashwat.pandey@gmail.com>
2020-06-26 20:19:38 +0530

    Update plane.py

--- userbot/plugins/plane.py
+++ userbot/plugins/plane.py
@@ -1,5 +1,4 @@
-"""By STARKTM1
-cmd : .plane"""
+#By STARKTM1
 from telethon import events
 import asyncio
 import os
@@ -28,4 +27,3 @@ async def _(event):
     await event.edit("-------------✈")
     await asyncio.sleep(3)
     await event.delete()
-
