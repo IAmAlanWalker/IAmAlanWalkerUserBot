bf8f0aeed0a9199348efe8bb0fc120e3c739fe76
CEO WHITE HAT CRACKS <official.shashwat.pandey@gmail.com>
2020-06-26 20:46:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,7 +10,7 @@ from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 pm_caption = "` Bot Made By @CeoWhiteHatCracks \n"
-pm_caption += "`Team Members Of This bot @spkal01 @sin_code @blackhathacker7\n"
+pm_caption += "`Team Members Of This bot @spkal01 @sin_code @blackhathacker7 @MrJetex\n"
 pm_caption += "`Sensible Userbot IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
@@ -20,10 +20,7 @@ pm_caption += "**Sensible Userbot OS** : `3.14`\n"
 pm_caption += "**Current Sat** : `Ceo White Hat Cracks `\n\n"
 pm_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
 pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "**License** : [MIT Licence]\n"
-pm_caption += "Copyright : By [Ceo White hat Cracks](GitHub.com/spandey112)\n"
 pm_caption += "[Deploy Sensible Userbot Made By @CEowhitehatcracks](https://github.com/spandey112/SensibleUserbot)"
-PM_IMG = "https://telegra.ph/file/2f76a5ea2a9ec22ab4284.jpg"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
