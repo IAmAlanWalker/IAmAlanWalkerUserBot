83c8289b32222e12c8fa317dedd9c1830376e05d
spandey112 <official.shashwat.pandey@gmail.com>
2020-07-15 20:53:03 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -52,7 +52,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                 await event.delete()
                 
                 
-              @bot.on (events.NewMessage(outgoing=True))
+              @bot.on(events.NewMessage(outgoing=True))
     async def on_outgoing_msg(event):
         if event.fwd_from:
             return
